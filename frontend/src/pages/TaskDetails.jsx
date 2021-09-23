3d0a39f9f1347bc9948618ba2ee4f19d0b6ee73e
matan3857 <matan38355@gmail.com>
2021-09-23 20:38:15 +0300

    task description update

--- frontend/src/pages/TaskDetails.jsx
+++ frontend/src/pages/TaskDetails.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Close } from '@mui/icons-material';
 import { TaskHeader } from '../cmps/TaskHeader';
 import { TaskCardCover } from '../cmps/TaskCardCover'
+import { TaskDescription } from '../cmps/TaskDescription'
 import { Popover } from '../cmps/Popover'
 export class TaskDetails extends Component {
   state = { isCover: true, currentTarget: null, isPopover: false }
@@ -27,7 +28,7 @@ export class TaskDetails extends Component {
           onClick={(event) => { this.setCurrentTarget(event); this.togglePopover() }}>click</button>
         <button
           onClick={(event) => { this.setCurrentTarget(event); this.togglePopover() }}>click</button>
-
+        <TaskDescription />
         {isPopover && currentTarget && <Popover togglePopover={this.togglePopover} currentTarget={currentTarget}></Popover>}
       </section >
 
