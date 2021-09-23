0e7ea54e7eb827dbcaf06570e72bd4d99adb61fc
bcd82 <barak.sidi@gmail.com>
2021-09-23 13:52:05 +0300

    created basic header

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export class Homepage extends Component {
     render() {
         return (
-            <div>
+            <section>
                 <h1>Homepage</h1>
-            </div>
+            </section>
         )
     }
 }
--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // const { Switch, Route } = ReactRouterDOM
-import { Switch, Route } from 'react-router'
+import { Switch, Route, Router } from 'react-router'
 
 import routes from './routes'
 import {AppHeader} from './cmps/AppHeader'
