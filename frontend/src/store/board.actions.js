431f318200185ddb592cf3ba9ff07cbcdefd56ed
bcd82 <barak.sidi@gmail.com>
2021-09-23 19:11:10 +0300

    starting board page

--- frontend/src/store/board.actions.js
+++ frontend/src/store/board.actions.js
@@ -4,7 +4,7 @@ export const loadBoards =()=> {
     return async (dispatch) => {
         try{
             const boards = await boardService.query()
-            console.log('board action',boards)
+            // console.log('loading boards',boards)
             dispatch({
                 type: "SET_BOARDS",
                 boards,
@@ -13,6 +13,23 @@ export const loadBoards =()=> {
         }
         catch(err){
             console.log('cant set boards',err)
+        }
+    }
+}
+
+export const loadBoard =(boardId)=> {
+    return async (dispatch) => {
+        try{
+            const board = await boardService.getById(boardId)
+            // console.log('loading board',board)
+            dispatch({
+                type: "SET_BOARD",
+                board,
+            });
+
+        }
+        catch(err){
+            console.log('cant set board',err)
         }
     }
 }
