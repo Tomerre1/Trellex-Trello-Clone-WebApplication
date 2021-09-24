c63cfdcc5001901e0dc13de70601fd7c921cdff2
Tomer <tomerevach@gmail.com>
2021-09-24 14:39:11 +0300

    merge conflict

--- frontend/src/cmps/Popover/Popover.jsx
+++ frontend/src/cmps/Popover/Popover.jsx
@@ -73,16 +73,12 @@ export class Popover extends React.Component {
                     style={this.getContentStyles()}
                 >
                     <div className="popover-header">
-<<<<<<< HEAD
-                        <h3> {title}</h3>
-=======
                         {/* <h3> {this.props.title}</h3> */}
                         {/* <h3>{title}</h3> */}
                         <span>{title}</span>
                         <button className="clean-btn" onClick={togglePopover}>
                             <CloseIcon />
                         </button>
->>>>>>> f149b5ca96474c4507ea9f303c4ab39cd5127875
                     </div>
                     <div className="popover-content">
                         {children}
