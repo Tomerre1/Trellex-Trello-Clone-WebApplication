f56ef3d984a72128ea21c3e91f2c9e9427c4f240
bcd82 <barak.sidi@gmail.com>
2021-09-24 10:52:07 +0300

    added menu icon(dots) to group preview header

--- frontend/src/cmps/GroupDetails.jsx
+++ frontend/src/cmps/GroupDetails.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { TaskPreview } from "./TaskPreview";
 import AddIcon from '@mui/icons-material/Add';
-
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 
 export function GroupDetails(props) {
   const { group, isAddNew } = props;
-  if (isAddNew) return <article className="group-details add-new"><div className="flex align-center"><AddIcon/>Add a new list</div></article>;
+  if (isAddNew) return <article className="group-details add-new"><div className="flex align-center"><AddIcon/>Add a list</div></article>;
   return (
     <article className="group-details flex column">
-      <div className="group-header">
+      <div className="group-header flex space-between">
         <p className="group-title flex column">{group.title}</p>
+        <MoreHorizOutlinedIcon/>
       </div>
       {group.tasks.map((task, idx) => (
         <TaskPreview task={task} key={idx} />
