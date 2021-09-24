3ea189699a493be2ffcb8820e147dc7f2a195fb9
bcd82 <barak.sidi@gmail.com>
2021-09-24 18:44:29 +0300

    added some css

--- frontend/src/cmps/Group/AddNewTask.jsx
+++ frontend/src/cmps/Group/AddNewTask.jsx
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import AddIcon from "@mui/icons-material/Add";
 
 export function AddNewTask() {
+    const [isClicked,setIsClicked] = useState(false)
+    const [taskTitle,setTaskTitle] = useState('')
   return (
     <div className="grp-add-task">
-      <AddIcon />
-      <p>Add new task</p>
+      <AddIcon className="add-icon"/>
+      <p>Add a task</p>
     </div>
   );
 }
