366f53c61b2087f4c919ac09bf35478d399ef29a
matan3857 <matan38355@gmail.com>
2021-09-28 19:11:57 +0300

    css update

--- frontend/src/cmps/TaskHeaderDate.jsx
+++ frontend/src/cmps/TaskHeaderDate.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import AddIcon from '@mui/icons-material/Add';
-
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 export function TaskHeaderDate({ selectedDate, setCurrentTarget }) {
     if (selectedDate.length === 0) return <></>
     return (
         <div className="task-details-header-date item-container flex column">
             <h3 className="task-details-header-title">DUE DATE</h3>
             <div className="date-container flex wrap">
+                <button>
                 <span 
+                className="date-context"
                 onClick={(event) => { setCurrentTarget(event, 'DATE'); }}>Oct 2 at 5:41 PM</span>
-                {/* {selectedLabels.map(label => {
-                    return <span
-                        onClick={(event) => { setCurrentTarget(event, 'LABELS'); }}
-                        key={label.id} className="label" style={{ backgroundColor: label.color }}>
-                        {label.title}
-                    </span>
-                })} */}
+                <span className="drop-down-icon"><ArrowDropDownIcon/></span>
+                </button>
             </div>
         </div>
     )
