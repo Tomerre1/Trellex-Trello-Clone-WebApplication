3fe8a7ff8757a565cd4022c428b13cf509f99730
matan3857 <matan38355@gmail.com>
2021-09-28 13:28:32 +0300

    progress bar style update

--- frontend/src/cmps/ProgressBar.jsx
+++ frontend/src/cmps/ProgressBar.jsx
@@ -4,22 +4,26 @@ import { Component } from "react";
 export class ProgressBar extends Component {
 
     state = {
-        completed : 0
+        completed: 0,
+        style: {
+            width: '',
+            backgroundColor: ''
+        }
     }
 
     componentDidMount = () => {
         const completed = this.props.doneTodosCalc()
-        this.setState(prevState => ({ ...prevState, completed }))
+        const backgroundColor = (completed === 100) ? '#61bd4f' : '#5ba4cf'
+        this.setState(prevState => ({ ...prevState, completed, style: { width: `${completed}%`, backgroundColor } }))
     }
-
     render() {
-        const { completed } = this.state
-        const width = `${completed}%`
+        const { style, completed } = this.state
+
         return (
-            <div className="checklist-progress">
+            <div className="checklist-progress" >
                 <span className="checklist-progress-percentage js-checklist-progress-percent">{completed}%</span>
                 <div className="checklist-progress-bar">
-                    <div className="checklist-progress-bar-current js-checklist-progress-bar" style={{ width }}>
+                    <div className="checklist-progress-bar-current js-checklist-progress-bar" style={style}>
 
                     </div>
                 </div>
