c81b0bf7f5c1e4b6d39515fa992f91b61005a006
matan3857 <matan38355@gmail.com>
2021-09-29 17:23:59 +0300

    user img added

--- frontend/src/cmps/AddComment.jsx
+++ frontend/src/cmps/AddComment.jsx
@@ -2,18 +2,32 @@ import React, { Component } from 'react';
 
 
 export class AddComment extends Component {
-    state ={
+    state = {
 
     }
 
 
-    render(){
-        console.log('loggedinUser',this.props.loggedinUser)
+    render() {
+        console.log('loggedinUser', this.props.loggedinUser)
+        const { loggedinUser } = this.props
         return (
-            <div className="comment-add flex">
+            <div className="add-comment flex">
+                <article className="member-wrapper">
+                    {loggedinUser?.imgUrl ? (
+                        <img
+                            src={loggedinUser.imgUrl}
+                            className="member-img"
+                            alt={"member-img"}
+                        />
+                    ) :
+                        (
+                            <div className="member-img" style={{ background: 'rgb(223, 225, 230)', color: 'inherit' }}>
+                                <p className={`member-letter preview`}>{loggedinUser?.fullname?.[0] || ''}</p>
+                            </div>
+                        )}
+                </article>
 
-
-
+                <div>Comment add</div>
 
             </div>
         )
