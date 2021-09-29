c263dff80a761ea2014f1ccc132649b657f9e4e9
matan3857 <matan38355@gmail.com>
2021-09-29 21:14:02 +0300

    activity preview update

--- frontend/src/cmps/ActivitiesList.jsx
+++ frontend/src/cmps/ActivitiesList.jsx
@@ -5,7 +5,9 @@ export function ActivitiesList({ CommAndAct }) {
     console.log('CommAndAct', CommAndAct)
     return (
         <div className="activities-list">
-            Activity list
+              {CommAndAct.map(activity => {
+                return <ActivityPreview key={activity.id} activity={activity} />
+            })}
 
         </div>
     )
--- frontend/src/cmps/ActivityPreview.jsx
+++ frontend/src/cmps/ActivityPreview.jsx
@@ -2,8 +2,31 @@ import React, { Component } from 'react'
 
 export class ActivityPreview extends Component {
     state = {}
-    
+
     render() {
-        return <div>Activity preview</div>
+        const { activity } = this.props
+        const {byMember} = activity
+        console.log('activity',activity)
+        return (
+            <div className="activity-preview flex">
+                 <article className="member-wrapper">
+                    {byMember?.imgUrl ? (
+                        <img
+                            src={byMember.imgUrl}
+                            className="member-img"
+                            alt={"member-img"}
+                        />
+                    ) :
+                        (
+                            <div className="member-img" style={{ background: 'rgb(223, 225, 230)', color: 'inherit' }}>
+                                <p className={`member-letter preview`}>{byMember?.fullname?.[0] || ''}</p>
+                            </div>
+                        )}
+                </article>
+
+
+
+            </div>
+        )
     }
 }
