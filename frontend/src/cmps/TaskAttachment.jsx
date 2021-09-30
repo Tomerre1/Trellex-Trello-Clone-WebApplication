47a22e1aa732713a17909721093c556cf64d540b
matan3857 <matan38355@gmail.com>
2021-09-30 23:20:41 +0300

    attach header

--- frontend/src/cmps/TaskAttachment.jsx
+++ frontend/src/cmps/TaskAttachment.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 export class TaskAttachment extends Component {
     state = {
@@ -15,6 +15,10 @@ export class TaskAttachment extends Component {
 
         return (
             <div className="task-attach">
+                <div className="window-modal-title flex align-center">
+                    <AttachFileIcon />
+                    <h3>Attachment</h3>
+                </div>
 
             </div>
         )
