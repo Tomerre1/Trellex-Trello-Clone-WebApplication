703c9651c168249212b3668e9c8b5b6e144f0ee0
Tomer <tomerevach@gmail.com>
2021-10-01 12:02:02 +0300

    upgrade popover cover

--- frontend/src/cmps/Popover/PopoverCover.jsx
+++ frontend/src/cmps/Popover/PopoverCover.jsx
@@ -132,23 +132,54 @@ export class PopoverCover extends React.Component {
         const { isHeaderSelected, isFullSelected, selectedColor, selectedPhoto } = this.state
         if (!selectedColor) return <div></div>
         const { togglePopover, currentTarget, title } = this.props
-        // <div className="header-section">
-        //                         <img src={selectedPhoto} style={{ objectFit: 'cover', height: '32px', objectPosition: 'top' }} alt="header" />
-        //                     </div>
         return (
             <Popover togglePopover={togglePopover} currentTarget={currentTarget} title={title} >
                 <div className="cover-popover" >
                     <h4>SIZE</h4>
                     <div className="cover-options flex space-between align-center">
                         <div className={`header-cover-preview ${isHeaderSelected ? 'selected' : ''}`} onClick={() => { this.setHeaderSelected() }}>
-                            {(selectedPhoto) ? <div className="header-section" style={{ backgroundImage: `url(${selectedPhoto}`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
-                                : <div className="header-section" style={{ backgroundColor: selectedColor }}></div>
+                            {(selectedPhoto) ?
+                                <>
+                                    <div className="header-section" style={{ backgroundImage: `url(${selectedPhoto}`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
+                                    <div style={{ padding: '6px 4px 4px 6px', position: 'relative' }}>
+                                        <div className="line" style={{ width: '122px' }}></div>
+                                        <div className="line" style={{ width: '98px', marginTop: '4px' }}></div>
+                                        <div className="dot"></div>
+
+                                    </div>
+                                </>
+                                :
+                                <>
+                                    <div className="header-section" style={{ backgroundColor: selectedColor }}>
+                                    </div>
+                                    <div style={{ padding: '6px 4px 4px 6px', position: 'relative' }}>
+                                        <div className="line" style={{ width: '122px' }}></div>
+                                        <div className="line" style={{ width: '98px', marginTop: '4px' }}></div>
+                                        <div className="dot"></div>
+                                    </div>
+                                </>
                             }
 
                         </div>
 
-                        {(selectedPhoto) ? <div onClick={() => this.setFullSelected()} className={`full-cover-preview ${isFullSelected ? 'selected' : ''}`} style={{ backgroundImage: `url(${selectedPhoto}`, backgroundSize: 'contain' }}></div> :
-                            <div onClick={() => this.setFullSelected()} className={`full-cover-preview ${isFullSelected ? 'selected' : ''}`} style={{ backgroundColor: selectedColor }}></div>
+                        {(selectedPhoto) ?
+
+                            <div onClick={() => this.setFullSelected()} className={`full-cover-preview ${isFullSelected ? 'selected' : ''}`} style={{ display: 'flex', backgroundImage: `url(${selectedPhoto}`, backgroundSize: 'contain' }}>
+                                <div style={{ padding: '6px 4px 4px 6px', position: 'relative', alignSelf: 'flex-end' }}>
+                                    <div className="line" style={{ width: '122px' }}></div>
+                                    <div className="line" style={{ width: '98px', marginTop: '4px' }}></div>
+                                </div>
+                            </div>
+
+                            :
+                            <>
+                                <div onClick={() => this.setFullSelected()} className={`full-cover-preview ${isFullSelected ? 'selected' : ''}`} style={{ backgroundColor: selectedColor, display: 'flex' }}>
+                                    <div style={{ padding: '6px 4px 4px 6px', position: 'relative', alignSelf: 'flex-end' }}>
+                                        <div className="line" style={{ width: '122px' }}></div>
+                                        <div className="line" style={{ width: '98px', marginTop: '4px' }}></div>
+                                    </div>
+                                </div>
+                            </>
                         }
 
                     </div>
