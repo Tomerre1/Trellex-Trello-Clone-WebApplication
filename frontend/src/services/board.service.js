4fa2e31700a77626d9df0cccc066f03883902a68
bcd82 <barak.sidi@gmail.com>
2021-10-02 16:55:36 +0300

    preview edit mode is cooler and title can be changed

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -1076,7 +1076,8 @@ export const boardService = {
     removeTask,
     addGroup,
     removeGroup,
-    createActivity
+    createActivity,
+    updateTaskByIds
 }
 async function query() {
     let boards = await storageService.query(STORAGE_KEY)
@@ -1152,6 +1153,8 @@ function updateTask(board, group, task) {
     board.groups[groupIndex].tasks[taskIndex] = task
     return { ...board }
 }
+
+
 
 async function addTask(taskTitle, boardId, groupId) {
     if (!taskTitle)
@@ -1229,6 +1232,21 @@ async function removeGroup(boardId, groupId) {
     }
 
 }
+async function updateTaskByIds(boardId,groupId,task) {
+    console.log(boardId, groupId, task)
+    const board = await getById(boardId)
+    const groupIdx = board.groups.findIndex(group => groupId === group.id)
+    const taskIdx = board.groups[groupIdx].tasks.findIndex(taskToFind => taskToFind.id === task.id)
+    board.groups[groupIdx].tasks[taskIdx] = task
+    save(board)
+    return board
+    
+}
+
+
+
+
+
 // function getEmptyBoard() {
 //     return {
 //         vendor: 'Susita-' + (Date.now() % 1000),
@@ -1252,8 +1270,3 @@ async function removeGroup(boardId, groupId) {
 //             _notifySubscribersBoardsChanged(boards)
 //         }) 
 // })
-
-
-
-
-
