7b5104df1d369285db9fb4768c847327f4acca78
bcd82 <barak.sidi@gmail.com>
2021-10-03 12:43:14 +0300

    added logo

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -3,67 +3,77 @@ import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { MemberList } from "../cmps/MemberList";
 import { BoardPreviewAdd } from "./Workspace/BoardPreviewAdd";
-import { addBoard } from '../store/board.actions'
+import { addBoard } from "../store/board.actions";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import AddIcon from "@mui/icons-material/Add";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
+import logo from "../assets/img/logo.png";
+
 function _AppHeader(props) {
   const [isCreateShown, setCreateShown] = useState(false);
-  const onAddBoard = async(title,bgClr,bgImg) => {
-    const board = await props.addBoard(title,bgClr,bgImg)
-    setCreateShown(false)
-    props.history.push(`/board/${board._id}`)
-}
+  const onAddBoard = async (title, bgClr, bgImg) => {
+    const board = await props.addBoard(title, bgClr, bgImg);
+    setCreateShown(false);
+    props.history.push(`/board/${board._id}`);
+  };
   return (
     <>
-    <header
-      className={`main-header flex align-center ${
-        props.location.pathname.includes("board") ? "on-board" : ""
-      }`}
-    >
-      <div className="header-btn-container flex">
-        <Link to="/" className="header-btn">
-          <HomeOutlinedIcon />
-        </Link>
-        <Link to="/workspace" className="header-btn boards">
-          <DashboardIcon className="icon" />
-          Boards
-        </Link>
-      </div>
-      <div>Trellex Logo</div>
-      <div className="header-btn-container flex">
-        <button
-          className="header-btn"
-          onClick={() => setCreateShown(!isCreateShown)}
-        >
-          <AddIcon />
-        </button>
+      <header
+        className={`main-header flex align-center ${
+          props.location.pathname.includes("board") ? "on-board" : ""
+        }`}
+      >
+        <div className="header-btn-container flex">
+          <Link to="/" className="header-btn">
+            <HomeOutlinedIcon />
+          </Link>
+          <Link to="/workspace" className="header-btn boards">
+            <DashboardIcon className="icon" />
+            Boards
+          </Link>
+        </div>
+          <img src={logo} alt="logo" className="logo" />
+        <div className="header-btn-container flex">
+          <button
+            className="header-btn"
+            onClick={() => setCreateShown(!isCreateShown)}
+          >
+            <AddIcon className="icon"/>
+          </button>
 
-        <button className="header-btn">
-          <NotificationsNoneIcon />
-        </button>
-        <MemberList members={[props.user]} />
-      </div>
+          <button className="header-btn">
+            <NotificationsNoneIcon className="icon"/>
+          </button>
+          <MemberList members={[props.user]} />
+        </div>
         <div
           className={`overlay ${isCreateShown ? "show" : ""}`}
-          onClick={()=>setCreateShown(false)}
-          ></div>
-    </header>
-{isCreateShown && <BoardPreviewAdd boardMode="true" setCreateShown={setCreateShown} onAdd={onAddBoard}/>}
-  </>
+          onClick={() => setCreateShown(false)}
+        ></div>
+      </header>
+      {isCreateShown && (
+        <BoardPreviewAdd
+          boardMode="true"
+          setCreateShown={setCreateShown}
+          onAdd={onAddBoard}
+        />
+      )}
+    </>
   );
 }
 
 function mapStateToProps(state) {
   return {
     user: state.userModule.loggedinUser,
-    boards:state.boardModule.boards
+    boards: state.boardModule.boards,
   };
 }
 const mapDispatchToProps = {
-  addBoard 
-}
+  addBoard,
+};
 
-export const AppHeader = withRouter(connect(mapStateToProps,mapDispatchToProps)(_AppHeader));
+export const AppHeader = withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(_AppHeader)
+);
