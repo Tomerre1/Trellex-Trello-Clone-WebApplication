38b2c7b247eb77b2a5e4ba75f998fd3e27255479
bcd82 <barak.sidi@gmail.com>
2021-10-03 22:41:27 +0300

    progress on profile popup, fixed task width css issue on scrollable group list

--- frontend/src/cmps/MemberPopup.jsx
+++ frontend/src/cmps/MemberPopup.jsx
@@ -6,7 +6,7 @@ export  const MemberPopup = ({member,togglePopOpen}) => {
     <div className="pop-overlay" style={{position:'fixed',top:0,right:0,inset:0,height:'100vh',width:'100%',backgroundColor:'transparent',zIndex:10}} onClick={()=>togglePopOpen(false)}></div>
       <div className="member-popup">
         {member?.imgUrl ? (
-          <img src={member.imgUrl} className="member-img" alt={"member-img"} />
+          <img src={member.imgUrl} className="member-img" alt="member-img" />
         ) : (
           <div
             className="member-img"
