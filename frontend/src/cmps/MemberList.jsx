36ceff2667e4a53a3f58cbf26a79a482656ee51a
bcd82 <barak.sidi@gmail.com>
2021-10-03 21:18:56 +0300

    reorganized member list + added member preview+ member popup

--- frontend/src/cmps/MemberList.jsx
+++ frontend/src/cmps/MemberList.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
-import AddIcon from '@mui/icons-material/Add';
-import { connect } from 'react-redux';
-import { setPosition, togglePopover } from '../store/app.actions';
+import React, { useEffect } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import {MemberPreview} from "./MemberPreview"
+import { connect } from "react-redux";
+import { setPosition, togglePopover } from "../store/app.actions";
+
 
 
 export function _MemberList(props) {
-  const { members, isInPreview, isInDetails, togglePopover } = props
+  const { members, isInPreview, isInDetails, togglePopover } = props;
   return (
-    <div className={`members ${isInPreview ? 'preview' : ''}`}>
+    <div className={`members ${isInPreview ? "preview" : ""}`}>
       {members &&
         members.map((member, idx) => (
-          <article key={idx} className="member-wrapper">
-            {member?.imgUrl ? (
-              <img
-                src={member.imgUrl}
-                className="member-img"
-                alt={"member-img"}
-              />
-            ) :
-              (
-                <div className="member-img" style={{ background: 'rgb(223, 225, 230)', color: 'inherit' }}>
-                  <p className={`member-letter ${isInPreview ? 'preview' : ''}`}>{member?.fullname?.[0].toUpperCase() || ''}</p>
-                </div>
-              )}
-          </article>
+          <MemberPreview member={member} isInPreview={isInPreview} key={idx}/>
+          // <article key={idx} className="member-wrapper">
+          //   {member?.imgUrl ? (
+          //     <img
+          //       src={member.imgUrl}
+          //       className="member-img"
+          //       alt={"member-img"}
+          //     />
+          //   ) : (
+          //     <div
+          //       className="member-img"
+          //       style={{ background: "rgb(223, 225, 230)", color: "inherit" }}
+          //     >
+          //       <p className={`member-letter ${isInPreview ? "preview" : ""}`}>
+          //         {member?.fullname?.[0].toUpperCase() || ""}
+          //       </p>
+          //     </div>
+          //   )}
+          // </article>
         ))}
-      {isInDetails &&
-        <button className="secondary-btn"
-          onClick={(event) => { props.setPosition({ pos: { pageX: event.pageX, pageY: event.pageY }, type: 'MEMBERS' }); togglePopover(); }}
-        ><AddIcon /></button>}
+      {isInDetails && (
+        <button
+          className="secondary-btn"
+          onClick={(event) => {
+            props.setPosition({
+              pos: { pageX: event.pageX, pageY: event.pageY },
+              type: "MEMBERS",
+            });
+            togglePopover();
+          }}
+        >
+          <AddIcon />
+        </button>
+      )}
     </div>
-  )
+  );
 }
-
-
-
 
 function mapStateToProps(state) {
   return {
@@ -43,12 +57,10 @@ function mapStateToProps(state) {
 }
 const mapDispatchToProps = {
   setPosition,
-  togglePopover
+  togglePopover,
 };
 
 export const MemberList = connect(
   mapStateToProps,
   mapDispatchToProps
 )(_MemberList);
-
-
