786f7b92aab3696e8da056a089a39cd594f879fe
bcd82 <barak.sidi@gmail.com>
2021-10-03 19:43:35 +0300

    sign up is functional

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { onLogin } from "../store/user.actions";
+import { onLogin, onSignup } from "../store/user.actions";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 
 function _LoginSignup(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
-  const onLogin = (ev) => {
+  const onSubmit = async (ev) => {
     ev.preventDefault();
     if (username.trim() && password.trim()) {
-      props.onLogin({ username, password });
+      if (fullname) {
+        await props.onSignup({ username, password, fullname });
+      } else {
+        props.onLogin({ username, password });
+      }
     }
+    props.history.push("/workspace");
   };
   return (
     <div className="login-signup  flex column align-center">
@@ -20,27 +27,41 @@ function _LoginSignup(props) {
         <h1>Trellex</h1>
       </div>
       <div className="login-container flex column">
-        <p>Log in to Trellex</p>
-        <form className="flex column" onSubmit={onLogin}>
+        {isLogin ? <p>Log in to Trellex</p> : <p>Sign up</p>}
+        <form className="flex column" onSubmit={onSubmit}>
           <input
             type="txt"
             value={username}
             onChange={(ev) => setUsername(ev.target.value)}
             placeholder="Enter Username"
           />
+          {!isLogin && (
+            <input
+              type="txt"
+              value={fullname}
+              onChange={(ev) => setFullname(ev.target.value)}
+              placeholder="Enter Full Name"
+            />
+          )}
           <input
             type="password"
             value={password}
             onChange={(ev) => setPassword(ev.target.value)}
-            placeholder="Enter password"
+            placeholder="Enter Password"
           />
-          <button className="login-submit" >Log in</button>
+          <button className="login-submit">
+            {isLogin ? "Log me in" : "Sign me up"}
+          </button>
         </form>
+        <p onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "Or sign up..." : "Back to Login"}
+        </p>
       </div>
     </div>
   );
 }
 const mapDispatchToProps = {
   onLogin,
+  onSignup,
 };
 export const LoginSignup = connect(null, mapDispatchToProps)(_LoginSignup);
