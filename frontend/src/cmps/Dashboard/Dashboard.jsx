85da64c61454ce07fe40c8a1452ef9529a9ab872
bcd82 <barak.sidi@gmail.com>
2021-10-05 18:41:12 +0300

    basic chart that shows todos vs done todos

--- frontend/src/cmps/Dashboard/Dashboard.jsx
+++ frontend/src/cmps/Dashboard/Dashboard.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
+import React from "react";
 import { connect } from "react-redux";
-import { Close } from '@mui/icons-material';
+import { Close } from "@mui/icons-material";
 
-import { DoughnutChart } from './DoughnutChart'
+import { DoughnutChart } from "./DoughnutChart";
+import { TodoChart } from "./TodoChart";
 
 export function _Dashboard(props) {
-
+ 
     const onBack = () => {
-        props.history.goBack()
-    }
-
-    return (
-        <section className="dashboard-overlay flex column">
-            <button onClick={onBack} class="close-btn clean-btn"><Close /></button>
-            <div className="dashboard-container flex column">
-                <div className="chart">
-                    <h1>Label statistics:</h1>
-                    <div>
-                        {/* <DoughnutChart board={board} /> */}
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+    props.history.goBack();
+  };
+  
+  const { board } = props;
+ 
+  return (
+    <section className="dashboard-overlay flex column">
+      <button onClick={onBack} class="close-btn clean-btn">
+        <Close />
+      </button>
+      <div className="dashboard-container flex column">
+        <div className="chart">
+          <h1>Label statistics:</h1>
+          <div>
+            {/* <DoughnutChart board={board} /> */}
+            <TodoChart board={{ ...board }} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function mapStateToProps(state) {
-    return {
-        board: state.boardModule.board,
-    };
+  return {
+    board: state.boardModule.board,
+  };
 }
-export const Dashboard = connect(
-    mapStateToProps,
-    null
-)(_Dashboard);
 
+export const Dashboard = connect(mapStateToProps, null)(_Dashboard);
