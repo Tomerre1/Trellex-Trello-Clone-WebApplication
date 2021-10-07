02ad718cfe97b661faad4b84a71fb6a594f39e2f
Tomer <tomerevach@gmail.com>
2021-10-08 02:52:59 +0300

    overlay working video

--- frontend/src/cmps/MediaRecord.jsx
+++ frontend/src/cmps/MediaRecord.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import { ReactMediaRecorder } from "react-media-recorder";
+import VideoRecorder from "react-video-recorder";
 import { cloudinaryService } from '../services/cloudinary-service'
 import { addTask } from '../store/board.actions';
 
@@ -17,31 +18,53 @@ export class _MediaRecord extends React.Component {
     setOverlay = (isOverlay) => {
         this.setState({ isOverlay })
     }
+    // this.props.type === 'video' && <ReactMediaRecorder
+    //     video
+    //     onStop={async (blobUrl, blob) => {
+    //         const res = await cloudinaryService.uploadFile(null, 'video', blob)
+    //         const videoUrl = res.secure_url
+    //         this.props.addTask('Media', this.props.boardId, this.props.groupId, null, videoUrl)
+    //     }
+    //     }
+
+    //     render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+    //         <div className="media-record">
+    //             <video controls src={mediaBlobUrl} />
+    //             <div className="flex media-btns">
+    //                 <button onClick={(ev) => { ev.stopPropagation(); startRecording(); }}><h1>Start Recording</h1></button>
+    //                 <button onClick={(ev) => { ev.stopPropagation(); stopRecording() }}>Stop Recording</button>
+    //             </div>
+
+    //         </div>
+    //     )}
+    // />
+
 
     render() {
         return (
-            <div className={`overlay ${this.state.isOverlay ? 'show' : ''}`} onClick={() => { this.setOverlay(false); this.props.setMediaType('') }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <>
                 {
-                    this.props.type === 'video' && < ReactMediaRecorder
-                        video
-                        onStop={async (blobUrl, blob) => {
-                            const res = await cloudinaryService.uploadFile(null, 'video', blob)
-                            const videoUrl = res.secure_url
-                            this.props.addTask('Media', this.props.boardId, this.props.groupId, null, videoUrl)
-                        }
-                        }
+                    <div className="media">
+                        {this.props.type === 'video' && <VideoRecorder
+                            className="video-recorder"
+                            isFlipped={false}
+                            // isOnInitially
+                            countdownTime={0}
+                            mimeType="video/webm;codecs=vp8,opus"
+                            constraints={{
+                                audio: true,
+                                video: {
+                                    resizeMode: "crop-and-scale"
+                                }
+                            }}
+                            onRecordingComplete={async (blob) => {
+                                const res = await cloudinaryService.uploadFile(null, 'video', blob)
+                                const videoUrl = res.secure_url
+                                this.props.addTask('Media', this.props.boardId, this.props.groupId, null, videoUrl)
+                            }}
+                        />}
+                    </div>
 
-                        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-                            <div className="media-record">
-                                <video controls src={mediaBlobUrl} />
-                                <div className="flex media-btns">
-                                    <button onClick={(ev) => { ev.stopPropagation(); startRecording(); }}><h1>Start Recording</h1></button>
-                                    <button onClick={(ev) => { ev.stopPropagation(); stopRecording() }}>Stop Recording</button>
-                                </div>
-
-                            </div>
-                        )}
-                    />
                 }
                 {
                     this.props.type === 'audio' && <ReactMediaRecorder
@@ -62,7 +85,8 @@ export class _MediaRecord extends React.Component {
                         )}
                     />
                 }
-            </div >
+                <div className={`overlay ${this.state.isOverlay ? 'show' : ''}`} onClick={() => { this.props.setMediaType('') }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}></div>
+            </>
         );
     }
 }
