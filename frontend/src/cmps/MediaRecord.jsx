648b7e322826e30c4677789883d75f6bada75b66
Tomer <tomerevach@gmail.com>
2021-10-08 12:43:42 +0300

    media record audio and video

--- frontend/src/cmps/MediaRecord.jsx
+++ frontend/src/cmps/MediaRecord.jsx
@@ -1,91 +1,119 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
-import { ReactMediaRecorder } from "react-media-recorder";
 import VideoRecorder from "react-video-recorder";
+import Grid from "@material-ui/core/Grid";
+import { ReactMic } from 'react-mic';
 import { cloudinaryService } from '../services/cloudinary-service'
 import { addTask } from '../store/board.actions';
+import StopIcon from "@material-ui/icons/Stop";
+import Fab from "@material-ui/core/Fab";
+import styled from "styled-components";
+import MicIcon from "@material-ui/icons/Mic";
+
+
 
 export class _MediaRecord extends React.Component {
     state = {
+        record: false,
         isOverlay: true
-    }
+    };
+
 
     componentDidMount() {
         this.setOverlay(true)
     }
 
+    startRecording = () => {
+        this.setState({
+            record: true
+        });
+    };
+
+    stopRecording = () => {
+        this.setState({
+            record: false
+        });
+    };
+
+    onStop = async (blob, type) => {
+        const res = await cloudinaryService.uploadFile(null, 'video', blob)
+        const url = res.secure_url
+        if (type === 'video') this.props.addTask('Media Video', this.props.boardId, this.props.groupId, null, url)
+        else this.props.addTask('Media Audio', this.props.boardId, this.props.groupId, url, null)
+        this.props.setMediaType('')
+    }
+
     setOverlay = (isOverlay) => {
         this.setState({ isOverlay })
     }
-    // this.props.type === 'video' && <ReactMediaRecorder
-    //     video
-    //     onStop={async (blobUrl, blob) => {
-    //         const res = await cloudinaryService.uploadFile(null, 'video', blob)
-    //         const videoUrl = res.secure_url
-    //         this.props.addTask('Media', this.props.boardId, this.props.groupId, null, videoUrl)
-    //     }
-    //     }
-
-    //     render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-    //         <div className="media-record">
-    //             <video controls src={mediaBlobUrl} />
-    //             <div className="flex media-btns">
-    //                 <button onClick={(ev) => { ev.stopPropagation(); startRecording(); }}><h1>Start Recording</h1></button>
-    //                 <button onClick={(ev) => { ev.stopPropagation(); stopRecording() }}>Stop Recording</button>
-    //             </div>
-
-    //         </div>
-    //     )}
-    // />
-
 
     render() {
+        const StyledFab = styled(Fab)`
+        position: relative;
+        background-color: ${!this.state.record ? "#272727" : "#8f3934"};
+        color: white;
+        opacity: 1;
+        margin: 12px;
+        z-index: 3;
+        transition: all 0.2s;
+        :hover {
+          background-color: ${!this.state.record ? "#272727" : "#8f3934"};
+          opacity: 0.9;
+        }
+      `;
+
+        const { setAudioPath } = this.props;
         return (
             <>
                 {
-                    <div className="media">
+                    <div className="media" style={{ width: "100%", maxWidth: 400, height: 400 }}>
                         {this.props.type === 'video' && <VideoRecorder
                             className="video-recorder"
                             isFlipped={false}
-                            // isOnInitially
                             countdownTime={0}
                             mimeType="video/webm;codecs=vp8,opus"
                             constraints={{
                                 audio: true,
                                 video: {
-                                    resizeMode: "crop-and-scale"
+                                    resizeMode: "crop-and-scale",
+                                    width: { exact: 480, ideal: 480 },
+                                    height: { exact: 640, ideal: 640 },
+                                    aspectRatio: { exact: 0.7500000001, ideal: 0.7500000001 },
                                 }
                             }}
-                            onRecordingComplete={async (blob) => {
-                                const res = await cloudinaryService.uploadFile(null, 'video', blob)
-                                const videoUrl = res.secure_url
-                                this.props.addTask('Media', this.props.boardId, this.props.groupId, null, videoUrl)
-                            }}
+                            onRecordingComplete={(blob) => this.onStop(blob, 'video')}
                         />}
                     </div>
 
                 }
                 {
-                    this.props.type === 'audio' && <ReactMediaRecorder
-                        audio
-                        onStop={async (blobUrl, blob) => {
-                            const res = await cloudinaryService.uploadFile(null, 'video', blob)
-                            const audioUrl = res.secure_url
-                            this.props.addTask('Audio', this.props.boardId, this.props.groupId, audioUrl, null)
-                        }
-                        }
+                    this.props.type === 'audio' && <div className="media">
+                        <ReactMic
+                            record={this.state.record}
+                            className="sound-wave"
+                            onStop={(blob) => this.onStop(blob, 'audio')}
+                            strokeColor="white"
+                            backgroundColor="black"
+                            setAudioPath={setAudioPath}
+                            height={240}
+                            width={400}
+                        />
+                        <Grid item container justify="center" xs={12}>
+                            {!this.state.record && <StyledFab
+                                onClick={this.startRecording}
+                                color="secondary" aria-label="record">
+                                <MicIcon />
+                            </StyledFab>}
+                            {this.state.record && <StyledFab color="secondary" aria-label="record"
+                                onClick={this.stopRecording}>
+                                <StopIcon />
+                            </StyledFab>}
+                        </Grid>
 
-                        render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-                            <div>
-                                <button onClick={(ev) => { ev.stopPropagation(); startRecording(); }}><h1>Start Recording</h1></button>
-                                <button onClick={(ev) => { ev.stopPropagation(); stopRecording() }}>Stop Recording</button>
-                                <audio controls src={mediaBlobUrl} />
-                            </div>
-                        )}
-                    />
+                    </div>
                 }
-                <div className={`overlay ${this.state.isOverlay ? 'show' : ''}`} onClick={() => { this.props.setMediaType('') }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}></div>
+                <div className={`overlay ${this.state.isOverlay ? 'show' : ''} `} onClick={() => { this.props.setMediaType('') }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}></div>
             </>
         );
     }
