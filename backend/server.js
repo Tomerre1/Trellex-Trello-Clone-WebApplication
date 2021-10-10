1f21985b7a964bd529e4dce29af0f09a2fbcc86b
matan3857 <matan38355@gmail.com>
2021-10-10 13:03:47 +0300

    limit update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const session = expressSession({
     cookie: { secure: false }
 })
 // Express App Config
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(session)
 app.use(express.static('public'))
 
